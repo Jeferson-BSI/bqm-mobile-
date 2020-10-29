087abcaafa8e721b0bba5240199df43911c2b752
Jeferson-BSI <jefersoncou18@gmail.com>
2020-10-29 03:28:05 -0300

    Add pagina inicial, E criação de novo componentes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
-// const HeaderStyle = () => (
-//     {
-//         title: 'BQM: BANCO DE QUESTÕES DE MATEMÁTICA',
-//         headerStyle: {
-//            backgroundColor: '#0b2639',
-//             //height: 40,
-//             //marginTop: 30,
-//             // para o headerText
-//             justifyContent: 'center',
-//             alignItems: 'center'
-    
-//         },
-//         headerTintColor: '#fff',
-//         headerTitleStyle: {
-//             fontSize: 14,
-//             color:'#fff'
-//         },
-//       }
-//     // <View style={styles.header}>
-//     // <Text style={styles.headerText}>BQM: BANCO DE QUESTÕES DE MATEMÁTICA</Text>
-//     // </View>
-//     );
-
 const Header = () => {
     return (
         <View style={styles.header}>
@@ -38,7 +15,6 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 40,//'35%',
         marginTop: 30,
-        // para o headerText
         justifyContent: 'center',
         alignItems: 'center'
     
