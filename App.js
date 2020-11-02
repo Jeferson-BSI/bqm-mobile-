a66ae9d62405bb9e1d4d2f3eaa9d471ee62e41e8
Isak Ruas <isakruas@gmail.com>
2020-11-02 00:34:12 -0300

    '01-11-2020-21h33'

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
+
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+
 import Routes from './src/routes/index';
 
 //import Entrar from './src/pages/Entrar';
@@ -8,21 +9,10 @@ import Routes from './src/routes/index';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-
-const Stack = createStackNavigator();
-
-
 function App() {
   return (
     <NavigationContainer>
         <Routes />
-        {/*<Stack.Navigator>
-        <Stack.Screen
-          name='Entrar'
-          component= { Entrar }
-          options={{ headerShown:false }}
-          />
-      </Stack.Navigator>*/}
     </NavigationContainer>
   );
 }
